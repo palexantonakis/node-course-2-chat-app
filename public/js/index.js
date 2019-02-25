7d7333999ee9b80826c85e28087c555a0170cac5
Your Name <you@example.com>
2019-02-26 01:40:37 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect',function () {
     console.log('Connected to server')
 
-    socket.emit('createMessage', {
-        from: 'Pavlos',
-        text: 'hey hey'
-    });
 });
 
 socket.on('disconnect',function (){
